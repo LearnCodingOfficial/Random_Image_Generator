73340f21a0588cf925713a2ccf950df5fac222b2
Learn Coding <90298313+LearnCodingOfficial@users.noreply.github.com>
2024-04-02 10:11:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const imgContainer = document.querySelector(".img-container")
+const btnEl = document.querySelector(".btn")
+
+
+btnEl.addEventListener("click", ()=>{
+    // console.log("you clicked me!")
+    imgNum = 6
+    updateImg()
+})
+
+function updateImg(){
+    for(let i = 0; i < imgNum; i++){
+        const newImg = document.createElement("img")
+        newImg.src = `https://picsum.photos/300?random=${Math.floor(Math.random()*1000)}`
+        imgContainer.appendChild(newImg)
+    }
+}
